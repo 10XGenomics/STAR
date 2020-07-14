1708112841448954d1523ce00987f62ad20e34dd
Lance Hepler <lance.hepler@10xgenomics.com>
2020-07-13 19:09:06 -0700

    forfeit STARsolo to bring back quantMode

--- source/Transcriptome_quantAlign.cpp
+++ source/Transcriptome_quantAlign.cpp
@@ -109,7 +109,6 @@ uint32 Transcriptome::quantAlign (Transcript &aG, Transcript *aTall, vector<uint
                     ++nAtr;
                     if (P.pSolo.strand==-1 || (int32) aTall[nAtr-1].Str == P.pSolo.strand) {//correct strand
                         readTranscripts.push_back(tr1);
-                        readTrGenes.insert(trGene[tr1]);
 //                         {//find the distance to 3'
 //                             uint64 distTTS=trLen[tr1]-(aTall[nAtr].exons[aTall[nAtr].nExons-1][EX_G] + aTall[nAtr].exons[aTall[nAtr].nExons-1][EX_L]);
 //                             cout <<"\t"<< distTTS;
